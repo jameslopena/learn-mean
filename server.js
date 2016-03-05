35c04bd52f57cde64f39ae9a267c36f8bec6a54d
James Lopena <lopena.james@gmail.com>
2016-03-05 01:08:05 +0100

    create angular application

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.use(stylus.middleware({
     compile: compile
 }));
 app.use(express.static(__dirname + '/public'));
+app.get('/partials/:partialPath', function(req, res){
+  res.render('partials/' + req.params.partialPath);
+});
 app.get('*', function(req, res){
     res.render('index');
 });
